e8e64e3cf9be65013203ce1c7a727b6802390478
David Fisher <fisherds@rose-hulman.edu>
2018-05-27 17:40:42 +0800

    finished demo app
    I don't love that this app is completely pointless.  Oh well.

--- PointOfSale/app/src/main/java/edu/rosehulman/fisherds/pointofsale/Item.java
+++ PointOfSale/app/src/main/java/edu/rosehulman/fisherds/pointofsale/Item.java
@@ -1,5 +1,7 @@
 package edu.rosehulman.fisherds.pointofsale;
 
+import android.graphics.Color;
+
 import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -9,52 +11,56 @@ import java.util.GregorianCalendar;
  * Created by boutell on 11/14/2015.
  */
 public class Item {
-    private String mName;
-    private int mQuantity;
-    private GregorianCalendar mDeliveryDate;
+  private String mName;
+  private int mQuantity;
+  private GregorianCalendar mDeliveryDate;
 
-    public Item() {
-        mName = "Nothing";
-        mQuantity = 0;
-        mDeliveryDate = new GregorianCalendar();
-    }
+  public Item() {
+    mName = "Nothing";
+    mQuantity = 0;
+    mDeliveryDate = new GregorianCalendar();
+  }
 
-    public Item(String name, int quantity, GregorianCalendar deliveryDate) {
-        mName = name;
-        mQuantity = quantity;
-        mDeliveryDate = deliveryDate;
-    }
+  public Item(String name, int quantity, GregorianCalendar deliveryDate) {
+    mName = name;
+    mQuantity = quantity;
+    mDeliveryDate = deliveryDate;
+  }
 
-    public static Item getDefaultItem() {
-        return new Item("Earplugs", 5, new GregorianCalendar());
-    }
+  public static Item getDefaultItem() {
+    return new Item("Earplugs", 5, new GregorianCalendar());
+  }
 
-    public String getName() {
-        return mName;
-    }
+  public static Item getEmptyItem() {
+    return new Item("---", 0, new GregorianCalendar());
+  }
 
-    public void setName(String name) {
-        mName = name;
-    }
+  public String getName() {
+    return mName;
+  }
 
-    public int getQuantity() {
-        return mQuantity;
-    }
+  public void setName(String name) {
+    mName = name;
+  }
 
-    public void setQuantity(int quantity) {
-        mQuantity = quantity;
-    }
+  public int getQuantity() {
+    return mQuantity;
+  }
 
-    public String getDeliveryDateString() {
-        return DateFormat.getDateInstance(DateFormat.MEDIUM).format(mDeliveryDate.getTime());
-                // DateFormat.getDateInstance().format(mDeliveryDate);
-    }
+  public void setQuantity(int quantity) {
+    mQuantity = quantity;
+  }
 
-    public long getDeliveryDateTime() {
-        return mDeliveryDate.getTime().getTime();
-    }
+  public String getDeliveryDateString() {
+    return DateFormat.getDateInstance(DateFormat.MEDIUM).format(mDeliveryDate.getTime());
+    // DateFormat.getDateInstance().format(mDeliveryDate);
+  }
 
-    public void setDeliveryDate(GregorianCalendar deliveryDate) {
-        mDeliveryDate = deliveryDate;
-    }
+  public long getDeliveryDateTime() {
+    return mDeliveryDate.getTime().getTime();
+  }
+
+  public void setDeliveryDate(GregorianCalendar deliveryDate) {
+    mDeliveryDate = deliveryDate;
+  }
 }
--- PointOfSale/app/src/main/java/edu/rosehulman/fisherds/pointofsale/MainActivity.java
+++ PointOfSale/app/src/main/java/edu/rosehulman/fisherds/pointofsale/MainActivity.java
@@ -1,15 +1,37 @@
 package edu.rosehulman.fisherds.pointofsale;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.ContextMenu;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
+
+  private static final String TAG = "PointOfSale";
+  private Item mCurrentItem;
+  private TextView mNameTextView, mQuantityTextView, mDateTextView;
+  private Item mClearedItem;
+  private ArrayList<Item> mItems = new ArrayList<>();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +40,95 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
+    mNameTextView = findViewById(R.id.name_text);
+    mQuantityTextView = findViewById(R.id.quantity_text);
+    mDateTextView = findViewById(R.id.date_text);
+
+    registerForContextMenu(mNameTextView);
+
+
+    mCurrentItem = new Item("Roses", 12, new GregorianCalendar());
+    mItems.add(mCurrentItem);
+    mCurrentItem = new Item("Tulips", 100, new GregorianCalendar());
+    mItems.add(mCurrentItem);
+    showCurrentItem();
+
+
     FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
+        addEditItem(false);
+
+        // This is temporary!
+//        mCurrentItem = Item.getDefaultItem();
+//        showCurrentItem();
       }
     });
+  }
+
+  private void addEditItem(final boolean isEdit) {
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    if (isEdit) {
+      builder.setTitle("Edit this item");
+    } else {
+      builder.setTitle("Add a new item");
+    }
+    View view = getLayoutInflater().inflate(R.layout.dialog_add, null, false);
+    builder.setView(view);
+    final EditText nameEditText = view.findViewById(R.id.edit_name);
+    final EditText quantityEditText = view.findViewById(R.id.edit_quantity);
+    final CalendarView deliveryDateView = view.findViewById(R.id.calendar_view);
+    final GregorianCalendar calendar = new GregorianCalendar();
+
+    if (isEdit) {
+      nameEditText.setText(mCurrentItem.getName());
+      quantityEditText.setText("" + mCurrentItem.getQuantity());
+      deliveryDateView.setDate(mCurrentItem.getDeliveryDateTime());
+    }
+
+    deliveryDateView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+      @Override
+      public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+        calendar.set(year, month, dayOfMonth);
+      }
+    });
+
+    builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        String name = nameEditText.getText().toString();
+        int quantity = 1;
+        try {
+          quantity = Integer.parseInt(quantityEditText.getText().toString());
+        } catch (Exception e) {
+          Log.e(TAG, "You failed to provide a quantity!");
+        }
+        mCurrentItem = new Item(name, quantity, calendar);
+        if (!isEdit) {
+          mItems.add(mCurrentItem);
+        }
+        Log.d(TAG, "items count = " + mItems.size());
+        Log.d(TAG, "items = " + mItems);
+        showCurrentItem();
+      }
+    });
+
+    builder.create().show();
+  }
+
+  private void showCurrentItem() {
+    mNameTextView.setText(mCurrentItem.getName());
+    mQuantityTextView.setText(getString(R.string.quantity_format, mCurrentItem.getQuantity()));
+    mDateTextView.setText(getString(R.string.date_format, mCurrentItem.getDeliveryDateString()));
+  }
+
+
+  @Override
+  public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+    super.onCreateContextMenu(menu, v, menuInfo);
+    getMenuInflater().inflate(R.menu.menu_context, menu);
+
   }
 
   @Override
@@ -37,16 +140,91 @@ public class MainActivity extends AppCompatActivity {
 
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
+    switch (item.getItemId()) {
+      case R.id.action_reset:
+        mClearedItem = mCurrentItem;
+//        mCurrentItem = new Item();
+        mCurrentItem = Item.getEmptyItem();
+        showCurrentItem();
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinator_layout), "Item cleared", Snackbar.LENGTH_LONG);
+        snackbar.setAction(R.string.undo, new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            mCurrentItem = mClearedItem;
+            mClearedItem = null;
+            showCurrentItem();
+            Snackbar.make(findViewById(R.id.coordinator_layout), "Item restored", Snackbar.LENGTH_LONG).show();
+          }
+        });
+        snackbar.show();
+        return true;
+      case R.id.action_clear_all:
+        showConfirmationDialog();
+        return true;
+      case R.id.action_search:
+        showSearchDialog();
+        return true;
+      case R.id.action_settings:
+        startActivity(new Intent(Settings.ACTION_SETTINGS));
+        return true;
     }
-
     return super.onOptionsItemSelected(item);
+  }
+
+  @Override
+  public boolean onContextItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.menu_context_edit:
+        addEditItem(true);
+        return true;
+      case R.id.menu_context_remove:
+        mItems.remove(mCurrentItem);
+        if (mItems.size() > 0) {
+          mCurrentItem = mItems.get(mItems.size() - 1);
+        } else {
+          mCurrentItem = Item.getEmptyItem();
+        }
+        showCurrentItem();
+        return true;
+    }
+    return super.onContextItemSelected(item);
+  }
+
+  private void showConfirmationDialog() {
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    builder.setTitle(getString(R.string.clear_all));
+    builder.setMessage("Are you sure you want to remove all the items?  This cannot be undone");
+    builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        mItems.clear();
+        mCurrentItem = Item.getEmptyItem();
+        showCurrentItem();
+      }
+    });
+    builder.create().show();
+  }
+
+  private void showSearchDialog() {
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    builder.setTitle(R.string.choose_an_item);
+    builder.setItems(getNames(), new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+       mCurrentItem = mItems.get(which);
+       showCurrentItem();
+      }
+    });
+    builder.setNegativeButton(android.R.string.cancel, null);
+    builder.create().show();
+
+  }
+
+  private String[] getNames() {
+    String[] names = new String[mItems.size()];
+    for (int i = 0; i < mItems.size(); i++) {
+      names[i] = mItems.get(i).getName();
+    }
+    return names;
   }
 }
